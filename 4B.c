f807cb1d21771ad244bc59680c5530f370facf99
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2024-10-04 22:07:07 +0530

    Fixed the program by adding flag for availibility of element condn

--- 4B.c
+++ 4B.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int a[7]={73, 18, 10, 5, 21, 32, 74}, n=7, i, data;
+    int a[7]={73, 18, 10, 5, 21, 32, 74}, n=7, i, data, found=0;
     printf(" Array A : ",a);
         for(i=0; i<n; i++)
             printf("\t %d", a[i]);
@@ -9,10 +9,13 @@ int main(){
             scanf("%d", &data);
             for(i=0; i<n; i++){
                 if(a[i]==data){
-                    printf("\n The Element Is Present At Position No : %d", i);
+                    printf(" The Element Is Present At Position No : %d", i);
+                    found=1;
 
                 }
             }
-        printf("The Element Is Not Present In Array!");
+            if(!found){
+                printf("The Element Is Not Present In Array!");
+            }
         return 0;
 }
