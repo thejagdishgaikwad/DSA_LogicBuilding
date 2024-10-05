f7c881d82b0928a4a6cce7976513df6664625b16
thejagdishgaikwad <111524423+thejagdishgaikwad@users.noreply.github.com>
2024-10-05 11:41:41 +0530

    Update 1A.c
    Removed unnecessary clutter

--- 1A.c
+++ 1A.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 void bubble_sort(int b[], int n);
 void reverse(int b[], int n);
 void search(int b[], int n);
-
 void main(){
     int a[7] = {10, 45, 51, 20, 75, 30, 35};
-    int i, ch = 1, n;
-    
+    int i, ch = 1, n;    
     n = 7;
     printf("\n A : ");
     for (i = 0; i < n; i++)
         printf("\t %d", a[i]);
-
     while (ch != 4) {
         printf("\n 1.Sort \n 2.Reverse \n 3.Searching \n 4.Exit \n Select the option : ");
         scanf("%d", &ch);
@@ -32,7 +28,6 @@ void main(){
         }
     }
 }
-
 void bubble_sort(int b[], int n) {
     int p, i, temp;
     for (p = 0; p < n - 1; p++) {
@@ -53,7 +48,6 @@ void bubble_sort(int b[], int n) {
         printf("\t %d", b[i]);
     }
 }
-
 void reverse(int b[], int n) {
     int i;
     printf("\n Array : ");
@@ -64,16 +58,13 @@ void reverse(int b[], int n) {
     for (i = n - 1; i >= 0; i--)
         printf("\t %d", b[i]);
 }
-
 void search(int b[], int n) {
     int data, i, flag = 0;
     printf("\n Array : ");
     for (i = 0; i < n; i++)
         printf("\t %d", b[i]);
-
     printf("\nEnter element to search: ");
     scanf("%d", &data);
-
     for (i = 0; i < n; i++) {
         if (b[i] == data) {
             printf("\n The element is present at position %d", i + 1);
@@ -81,7 +72,6 @@ void search(int b[], int n) {
             break;
         }
     }
-
     if (flag == 0) {
         printf("\n The element is not present in the array!!");
     }
